4b4f1d2835f354578c0ced7325104a74d5a7ef61
suzuki-yoshiki <yoshisu19950418@yahoo.co.jp>
2020-06-24 23:16:25 +0900

    Finish editing-users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_24_044409) do
+ActiveRecord::Schema.define(version: 2020_06_24_140450) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_06_24_044409) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
