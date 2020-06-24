0bdf4a6b62900179a327e12cb7345f67b5e42198
suzuki-yoshiki <yoshisu19950418@yahoo.co.jp>
2020-06-24 23:34:18 +0900

    Finish japanization-and-time-format

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.5.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
+gem 'rails-i18n'
 gem 'bcrypt'
 gem 'faker'
 gem 'bootstrap-sass'
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module ScheduleApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.time_zone = 'Asia/Tokyo'
+    config.i18n.default_locale = :ja # デフォルトの言語を日本語に設定します
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
